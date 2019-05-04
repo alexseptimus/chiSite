aa137acaddbe6df5d96fbeadc9c9f0faa1d2eb50
alexseptimus <alexboccia1@gmail.com>
2019-05-05 01:54:30 +0200

    Add files via upload

--- script/pages.js
+++ script/pages.js
@@ -19,4 +19,47 @@ function switchcontent(event){
 }
 
 
+function hover(element){
+    element.setAttribute('src',"asset/paypalover.jpg");
+}
+function unhover(element){
+    element.setAttribute('src',"asset/paypal.jpg");
+}
 
+
+/** Photo gallery script */
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("photos");
+  var dots = document.getElementsByClassName("indicatorPhotos");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" activephoto", " transparentphoto");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " activephoto";
+}
+
+/*
+AUTO SHOW
+var myVar = setInterval(scrollphoto, 15000);
+
+function scrollphoto() {
+  plusSlides(1);
+}*/
